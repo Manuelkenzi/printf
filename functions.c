fe166152c8daeb560948259bb8571fb6f766a17a
sassykimxos <110163464+sassykimxos@users.noreply.github.com>
2022-10-19 00:06:17 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -153,8 +153,7 @@ int print_int(va_list types, char buffer[],
  * Return: Numbers of char printed.
  */
 int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
+	int flags, int width, int precision, int size) {
 	unsigned int n, m, i, sum;
 	unsigned int a[32];
 	int count;
